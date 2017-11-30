19280a8f1ac999d5281d009b40e761bd141dddea
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-30 11:40:58 +0100

    Add files via upload

---
+++ suma mia.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int vector1[5];
+    int vector2[5];
+    int vectort[5];
+    int salir;
+    int cont;
+    for(int cont=0;cont<5;cont++){
+    cout<<"vector["<<cont<<"]=";
+    cin>>vector1[cont];
+    }
+    cout<<"presiona para salir y luego ENTER";
+    cin>>salir;
+    return salir;
+}
+
+                              
